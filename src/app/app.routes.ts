696825848f07c20895669206445d495aa7f3235a
RomanMykush <roman.mykush.dev@gmail.com>
2024-08-16 11:33:26 +0300

    Add country details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'countries/:code', component: CountryDetailsComponent },
+];
---
+++ src/app/country-details/country-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CountryService } from '../shared/services/country.service';
+import { CountryInfo } from '../shared/models/country-info';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-country-details',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './country-details.component.html',
+})
+export class CountryDetailsComponent implements OnInit {
+  countryCode: string = '';
+  country: CountryInfo | null = null;
+  selectedYear: number = new Date().getFullYear();
+
+  constructor(
+    private route: ActivatedRoute,
+    private countryInfoService: CountryService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.countryCode = params['code'];
+      this.country = null;
+      this.selectedYear = new Date().getFullYear();
+
+      this.countryInfoService
+        .fetchFullCountryInfo(this.countryCode)
+        .subscribe(res => {
+          this.country = res;
+        });
+    });
+  }
+
+  range(start: number, end: number): number[] {
+    return Array.from(Array(end - start + 1).keys()).map(x => x + start);
+  }
+}
