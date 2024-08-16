00948bd374f57c57af0145fc31a8e78e6d9d787d
RomanMykush <roman.mykush.dev@gmail.com>
2024-08-16 13:57:31 +0300

    Add next holiday card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { NextHolidayComponent } from './next-holiday/next-holiday.component';
+import { CountryService } from '../shared/services/country.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, NextHolidayComponent],
   templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  countryCodes: string[] = [];
+
+  constructor(private countryService: CountryService) {}
+
+  ngOnInit() {
+    this.countryService.countryInfos$.subscribe(res => {
+      const array = this.shuffle(res);
+      this.countryCodes = array.slice(0, 3).map(c => c.countryCode);
+    });
+  }
+
+  shuffle<T>(array: T[]): T[] {
+    const shuffledArray = array.slice();
+    let currentIndex = shuffledArray.length;
+
+    while (currentIndex != 0) {
+      const randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
+        shuffledArray[randomIndex],
+        shuffledArray[currentIndex],
+      ];
+    }
+    return shuffledArray;
+  }
+}
---
+++ src/app/home/next-holiday/next-holiday.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CountryService } from '../../shared/services/country.service';
+import { CountryInfo } from '../../shared/models/country-info';
+import { HolidayService } from '../../shared/services/holiday.service';
+import { Holiday } from '../../shared/models/holiday';
+import { MatCardModule } from '@angular/material/card';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-next-holiday',
+  standalone: true,
+  imports: [MatCardModule, DatePipe],
+  templateUrl: './next-holiday.component.html',
+})
+export class NextHolidayComponent implements OnInit {
+  @Input() countryCode: string = '';
+
+  country: CountryInfo | null = null;
+  holiday: Holiday | null = null;
+
+  constructor(
+    private countryService: CountryService,
+    private holidayService: HolidayService
+  ) {}
+
+  ngOnInit() {
+    this.countryService
+      .fetchFullCountryInfo(this.countryCode)
+      .subscribe(res => (this.country = res));
+
+    this.holidayService
+      .fetchNextHolidays(this.countryCode)
+      .subscribe(res => (this.holiday = res[0]));
+  }
+}
